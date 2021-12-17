817c9a06d347ad6db0ef5c67a3c676e75c34ebd2
nasty <anastasiia_balzamova@epam.com>
2021-12-17 13:12:09 +0300

    remove all files from root dir

--- src/config/status-codes.ts
+++
@@ -1,6 +0,0 @@
-export enum StatusCodes {
-  Ok = 200,
-  NotFound = 404,
-  BadRequest = 400,
-  Error = 500,
-}
--- src/interfaces/ICard.ts
+++
@@ -1,8 +0,0 @@
-export default interface ICard {
-  id: number,
-  categoryId: number,
-  word: string,
-  translation: string,
-  image: string,
-  audio: string,
-}
